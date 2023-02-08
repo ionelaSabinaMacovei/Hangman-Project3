bd5dff7f41ef64acbb2038d5db5efeff196f9c88
ionelaSabinaMacovei <badale.sabina@yahoo.com>
2023-02-08 14:20:38 +0000

    Add the result function for display the win/lose message

--- run.py
+++ run.py
@@ -196,6 +196,61 @@ def play_game(word):
     print(display_hangman(lives))
     word_space(f"\t{full_word}")
     print("\n")
+    while not guessed and lives > 0:
+        print(f"\n\tWRONG LETTERS GUESSED:\n\t{guessed_wrong}\n")
+        display_score(score)
+        print("\n")
+        if lives > 1:
+            print(f"\n\tYOU HAVE {lives} LIVES")
+        else:
+            print(f"\n\tYOU HAVE {lives} LIVES LEFT\n")
+        guess = input(f"""\t\t
+        GUESS A LETTER OR A WORD PLEASE:\n\t>>> """).upper()
+        print("\n")
+        #Check if the player has already guess the letter
+        # Or if the letter guessed in not in the word
+        # And if the letter guessed is in the word
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guessed_letters:
+                print(f"""\n\t
+                YOU HAVE ALREADY GUESSED THIS LETTER {guess}\n""")
+            elif guess not in word:
+                print(f"""\n\t
+                {guess} IS NOT IN THE WORD. TRY ANOTHER ONE!\n""")
+                lives -= 1
+                guessed_letters.append(guess)
+                guessed_wrong.append(guess)
+            else:
+                print(f"""\n\t
+                GREAT, {guess} IS IN THE WORD! KEEP GOING!\n""")
+                guessed_letters.append(guess)
+                guessed_right += 1
+                score += CORRECT_ANSWER
+                word_as_list = list(full_word)
+                indices = [i for i, letter in enumerate(
+                          word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess
+                full_word = "".join(word_as_list)
+                if "_" not in full_word:
+                    guessed = True
+        elif len(guess) == len(word) and guess.isalpha():
+            if guess in guessed_words:
+                print(f"""\n\t
+                YOU HAVE GUESSED THE WORD {guess} ALREADY.""")
+            elif guess != word:
+                print(f"\n\t{guess}, IS NOT THE WORD. TRY AGAIN!")
+                lives -= 1
+                guessed_words.append(guess)
+            else:
+                guessed = True
+                full_word = word
+        else:
+            print(f"\n\tIS NOT VALID GUESS.\n")
+        print(display_hangman(lives))
+        word_space(f"\t{full_word}")
+        print("\n")
+    final_result(guessed, word, guessed_right, score)
 
 
 def word_space(full_word):
@@ -213,3 +268,27 @@ def display_score(score):
     print(f"\tSCORE: {score}")
 
 
+def final_result(guessed, random_word, guessed_right, score):
+    """
+    Check if the player loses or won the game guessing the word letter
+    by letter or the word at once
+    """
+    if guessed and len(random_word) >= 6 and guessed_right <= 3:
+        print(f"{Fore.GREEN}{hangman_logo[3]}")
+        print(f"""{Fore.GREEN}
+        YOU WIN {player_name}, YOU HAVE GUESSED THE WORD COMPLETELY AT ONCE!\n
+        """)
+        score = score + EXTRA_SCORE + FULLY_WORD_SCORE
+    elif guessed:
+        print(f"{Fore.GREEN}{hangman_logo[2]}")
+        print(f"""{Fore.GREEN}
+        YOU WIN {player_name}, YOU HAVE GUESSED THE RIGHT WORD!\n
+        """)
+        score = score + EXTRA_SCORE
+    else:
+        print(f"{Fore.RED}{hangman_logo[1]}")
+        print(F"""{Fore.RED}
+        YOU LOSE {player_name}, THE RIGHT WORD WAS {random_word}!
+        """)
+    update_worksheet(data, score)
+    display_score(score)
