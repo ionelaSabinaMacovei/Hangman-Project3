646d9ce4e6534e6dc3ceac0fe0359c97501871fb
ionelaSabinaMacovei <badale.sabina@yahoo.com>
2023-02-13 14:30:49 +0000

    Update the list of the words

--- words.py
+++ words.py
@@ -1,1 +1,6 @@
-words = ['']
+words = ['tirana', 'vienna', 'minsk', 'brussels', 'sarajevo', 'sofia',
+         'zagreb', 'prague', 'copenhagen', 'tallinn', 'helsinki', 'paris', 
+         'berlin', 'athens', 'budapest', 'dublin', 'rome', 'riga', 'vaduz', 
+         'vilnius', 'luxembourg', 'chisinau', 'oslo', 'warsaw', 'lisbon',
+         'bucharest', 'moscow', 'belgrade', 'bratislava', 'ljubljana', 
+         'madrid', 'stockholm', 'kiev', 'london']
